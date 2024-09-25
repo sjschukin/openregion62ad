a9c957b5f0992b396e1fa412867ce485aa4aa73f
Sergey Shchukin <sergey_shchukin@rntgroup.com>
2024-09-25 11:14:39 +0300

    Added file parser

---
+++ src/OpenRegion62Ad/JsonDownloader.cs
@@ -0,0 +1,17 @@
+﻿using OpenRegion62Ad.Entities;
+using System.Net.Http.Json;
+
+namespace OpenRegion62Ad;
+
+internal class JsonDownloader
+{
+	private static readonly HttpClient _client;
+
+    static JsonDownloader()
+    {
+        _client = new HttpClient();
+    }
+
+	public static Task<IReadOnlyCollection<EventEntity>?> GetDataAsync(Uri requestUri, CancellationToken cancellationToken)
+        => _client.GetFromJsonAsync<IReadOnlyCollection<EventEntity>>(requestUri, cancellationToken);
+}

734e61f37dd6eb936278a8641dabd37159aad310
Sergey Shchukin <sergey_shchukin@rntgroup.com>
2024-09-25 11:14:14 +0300

    Added parameters supporting

---
+++ src/OpenRegion62Ad/ConverterParameters.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenRegion62Ad;
+
+internal class ConverterParameters
+{
+	public required Uri Uri { get; init; }
+	public required string FileName { get; init; }
+}
---
+++ src/OpenRegion62Ad/ConverterParametersParser.cs
@@ -0,0 +1,27 @@
+﻿namespace OpenRegion62Ad;
+
+internal class ConverterParametersParser
+{
+	private const string URI_ARG = "--uri";
+	private const string FILE_ARG = "--file";
+
+	private static readonly string[] _requiredArgs = [URI_ARG, FILE_ARG];
+
+	public static ConverterParameters? Parse(string[] args)
+	{
+		if (args.Length < 4)
+			return null;
+
+		if (!args.All(item => _requiredArgs.Contains(item)))
+			return null;
+
+		int uriIndex = Array.IndexOf(args, URI_ARG);
+		int fileIndex = Array.IndexOf(args, FILE_ARG);
+
+		return new ConverterParameters
+		{
+			Uri = new Uri(args[uriIndex + 1]),
+			FileName = args[fileIndex + 1]
+		};
+	}
+}
